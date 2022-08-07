e62fb37649ad309236d441d47d2b4fed894662a3
Shreyas Gavhalkar <shreyas@pop-os.localdomain>
2022-08-07 14:36:12 +0530

    added final page with table, everything works

--- defense_project/defense/models.py
+++ defense_project/defense/models.py
@@ -4,6 +4,7 @@ class AddParticipant(models.Model):
     personnel_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=200)
     photo = models.TextField()
+    # sort_id = models.AutoField(default=1)
     def __str__(self):
         return self.name+"  (id= "+str(self.personnel_id)+")"
 
@@ -14,7 +15,15 @@ class ActivityModels(models.Model):
     activity2 = models.BooleanField()
     activity3 = models.BooleanField()
     activity4 = models.BooleanField()
-    photo = models.TextField()
+    photo = models.TextField(unique=True)
 
-
+class ActivityReport(models.Model):
+    id = models.ForeignKey(AddParticipant, to_field = "personnel_id", db_column="participant_id" ,primary_key=True, on_delete=models.CASCADE)
+    photo = models.ForeignKey(ActivityModels, to_field = "photo", db_column="photo", on_delete=models.CASCADE )
+    name = models.CharField(max_length=200)
+    activity1 = models.CharField(max_length=200, default="NP")
+    activity2 = models.CharField(max_length=200, default="NP")
+    activity3 = models.CharField(max_length=200, default="NP")
+    activity4 = models.CharField(max_length=200, default="NP")
+    salute_angle = models.FloatField()
 # Create your models here.
--- defense_project/defense/views.py
+++ defense_project/defense/views.py
@@ -4,22 +4,26 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import ActivityForm, AddParticipantForm
-from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from defense.models import AddParticipant
 from django.contrib.auth.forms import AuthenticationForm, authenticate
 from django.contrib.auth import login
 from . import Image_Salute
+from defense.models import ActivityModels, ActivityReport, AddParticipant
 # from .forms import UserCreation
   
 import logging
 import pdb
 
 
+def index(request):
+    return redirect("defense:login")
+
 def auth(request):#, username, password):
     
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
+        pdb.set_trace()
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
@@ -57,13 +61,13 @@ def activity(request):
     if not request.user.is_authenticated:
         return redirect("defense:login")
     form = ActivityForm()
-    # pdb.set_trace()
+    
     context ={"form":ActivityForm()}
     # pdb.set_trace()
     if request.method == 'GET':
         return render(request, 'defense/activity.html', context)
     else:
-        # pdb.set_trace()
+        pdb.set_trace()
         form = ActivityForm(request.POST)
         # form = ActivityForm(instance = a)
         form.save()
@@ -73,7 +77,6 @@ def add_participant(request):
     # pdb.set_trace()
     if not request.user.is_authenticated:
         return redirect("defense:login")
-    # pdb.set_trace()
     if request.method == 'GET':
         context = {'form':AddParticipantForm()}
         items = AddParticipant.objects.all().order_by('-id')[:5]
@@ -90,8 +93,28 @@ def add_participant(request):
 def generate_report(request, personnel_id):
     if not request.user.is_authenticated:
         return redirect("defense:login")
-    Output = "at report gen page with id "+str(personnel_id)
-    return render(request, 'defense/final_report.html', )
+    details = ActivityModels.objects.get(pk=personnel_id)
+    results = ActivityReport()
+    # print("*******************************\n")
+    # print(details.activity1)
+    # Output = "at report gen page with id "+str(personnel_id)
+    photo = details.photo
+    ref_participant = AddParticipant.objects.all().filter(personnel_id=personnel_id)
+    # ref_participant_activiy = ActivityModels.objects.get(pk=personnel_id)
+    results.photo = details
+    results.id = ref_participant[0]
+    results.name = ref_participant[0].name
+    print(ref_participant)
+    # results.id = AddParticipant.objects.get(pk=personnel_id)
+    if(details.activity1):
+        photo = photo.split(',')[1]
+        activity1_results = Image_Salute.run(photo)
+        results.activity1 = activity1_results[0]
+        results.salute_angle = activity1_results[1]
+    context = results.__dict__
+    print(context.keys())
+    results.save()
+    return render(request, 'defense/final_report.html',context )
 
 
 # Create your views here.
